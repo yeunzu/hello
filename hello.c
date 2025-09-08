77fbfe029b70c3636ca97fe79e502704d404afb9
Najang06 <yj060706@gmail.com>
2025-09-08 14:05:00 +0900

    2025-09-08 1교시 수업내용

--- hello.c
+++ hello.c
@@ -1,7 +1,22 @@
 # include <stdio.h>
 
-int main(void)
-{
-	printf("Hello World!");
-	return;
+int main(void) {
+	//int x;
+	//int y;
+	//int sum;
+	//x = 100;
+	//y = 200;
+	//sum = x + y;
+	//printf("두 수의 합 : %d\n", sum);
+	//return 0;
+
+	int x, y, sum;
+	x = 100, y = 200;
+	/*
+	* 이건 컴파일 안된다.
+	* x, y = 100, 200; (파이썬에서나 가능한 짓)
+	*/
+	sum = x + y;
+	printf("두 수의 합 : %d \n", sum);
+	return 0;
 }
